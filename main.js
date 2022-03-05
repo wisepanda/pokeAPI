0a65eb5aaee4892ebd4e979f36299b19dc5dcca3
wisepanda <93228256+wisepanda@users.noreply.github.com>
2022-03-05 13:39:58 +0000

    fetching all the data

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 
 let pokeImage = document.querySelector("#pokemonPic")
 let pokeName = document.querySelector("#pokemon-name")
+let pokedexNo = document.querySelector("#pokedexNo")
 let pokeType = document.querySelector("#pokemon-type")
+let pokeHeight = document.querySelector("#pokeHeight")
+let pokeWeight = document.querySelector("#pokeWeight")
+let pokeHp = document.querySelector("#pokeHp")
+let pokeAttack = document.querySelector("#pokeAttack")
+let pokeDefense = document.querySelector("#pokeDefense")
+let pokeSpecialAttack=document.querySelector("#pokeSpecial-attack")
+let pokeSpecialDefense=document.querySelector("#pokeSpecial-defense")
+let pokeSpeed=document.querySelector("#pokeSpeed")
+
 
         
     async function getPokemon() {
@@ -13,10 +23,28 @@ let pokeType = document.querySelector("#pokemon-type")
         pokeImage.src = pokemon.sprites.other.dream_world.front_default;
         pokeName.innerHTML = pokemon.name;
         pokeType.innerHTML =  pokemon.types[0].type.name;
+        pokedexNo.innerHTML = "pokedex No: " + pokemon.id;
+        pokeHeight.innerHTML = "Height: " + pokemon.height;
+        pokeWeight.innerHTML = "Weight: " + pokemon.weight;
+        pokeHp.innerHTML = "Hp: " + pokemon.stats[0].base_stat;
+        pokeAttack.innerHTML = "Attack: " + pokemon.stats[1].base_stat;
+        pokeDefense.innerHTML = "Defense: " + pokemon.stats[2].base_stat;
+        pokeSpecialAttack.innerHTML = "Special Attack: " + pokemon.stats[3].base_stat;
+        pokeSpecialDefense.innerHTML = "Special Defense: " + pokemon.stats[4].base_stat;
+        pokeSpeed.innerHTML = "Speed: " + pokemon.stats[5].base_stat;
+
     }
     getPokemon();
     
-document.querySelector("#button").addEventListener("submit", getPokemon);
+    // pokem.stats.map(([
+    //     stat.name,
+    //     stat.
+    // ]))
+
+
+
+
+document.querySelector("#button").addEventListener("click", getPokemon);
 
 
 
